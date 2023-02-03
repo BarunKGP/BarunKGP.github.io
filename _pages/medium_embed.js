8d3a5a96e5cad8bdf54a1f0368aeca7194c7b4c5
Das, Barun <bdas31@gatech.edu>
2023-02-03 18:03:00 -0500

    added medium embed

---
+++ _pages/medium_embed.js
@@ -0,0 +1,7 @@
+<div id="retainable-rss-embed" data-rss="https://medium.com/@barundas_79052/why-you-should-use-lens-the-kubernetes-ide-an-introduction-51e30576bba0"
+data-maxcols="3" 
+data-layout="grid" 
+data-poststyle="inline" 
+data-readmore="Read the rest" 
+data-buttonclass="btn btn-primary" 
+data-offset="-100"></div>
